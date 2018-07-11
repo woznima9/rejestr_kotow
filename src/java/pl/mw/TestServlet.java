1825d2049ea6445a216a3eb2ee01ec653325922b
woznima9 <37245064+woznima9@users.noreply.github.com>
2018-07-11 17:02:39 +0200

    Servlet Java z wykorzystaniem Tomcat - ćwiczenie_MW

---
+++ src/java/pl/mw/TestServlet.java
@@ -0,0 +1,21 @@
+package pl.mw;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", value=("/tese"))
+public class TestServlet extends HttpServlet {
+    private static final long serialVersionUID=1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Żądanie GET przetworzono pomyślnie");
+    }
+}
---
+++ src/main/webapp/start.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>STAóRTowa</title>
+</head>
+<body>
+<h2>Hello World!</h2>
+<div>Uruchomienie strony po raz pierwszy</div>
+<div>Kolejna linijka</div>
+<ul>
+    <li>pierwszy</li>
+    <li><a href="/star/index.html">START</a></li>
+
+</ul>
+<div>
+    <p>Test znaków POLSKICH Łódź</p></div>
+
+</body>
+</html>
